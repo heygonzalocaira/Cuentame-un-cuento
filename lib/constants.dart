2a69f457281152b70f65a221d0a6c9d4d4e8f4be
GonzaloCaira <gcairaaguilar@gmail.com>
2020-12-02 19:45:16 -0500

    Main screen

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  final double widthScreen;
+  final double heightScreen;
+  Constants(
+    this.widthScreen,
+    this.heightScreen,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,16 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,22 +29,103 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
+  @override
+  dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double widthScreen;
+    double heightScreen;
+    widthScreen = MediaQuery.of(context).size.width;
+    heightScreen = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Text(
-              "Arma tu cuento")), // This trailing comma makes auto-formatting nicer for build methods.
+      body: SafeArea(
+          child: Center(
+              child: Stack(
+        children: <Widget>[
+          Container(
+              width: widthScreen,
+              height: heightScreen,
+              color: Colors.black,
+              child: Image.asset(
+                'assets/init/background.png',
+                fit: BoxFit.fill,
+              )),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+                width: 250,
+                height: 150,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/init/arma.png',
+                  fit: BoxFit.fill,
+                )),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: IconButton(
+              icon: new Icon(
+                Icons.play_circle_fill,
+                size: 100,
+              ),
+              highlightColor: Colors.lightBlue,
+              onPressed: () {
+                print("ah prro");
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                width: 80,
+                height: 100,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/init/sonder.png',
+                  fit: BoxFit.fill,
+                )),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+                width: 80,
+                height: 80,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/init/cs.png',
+                  fit: BoxFit.fill,
+                )),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+                width: 80,
+                height: 80,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/init/unsa.png',
+                  fit: BoxFit.fill,
+                )),
+          ),
+        ],
+      ))),
     );
   }
 }
