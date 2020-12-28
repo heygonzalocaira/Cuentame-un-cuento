2a2ab26becd38216aea56aee8c732f0422cf3390
GustavoIncaCh <70337161+GustavoIncaCh@users.noreply.github.com>
2020-12-28 18:31:01 -0500

    ConstantsAudios
    Audios agregados

---
+++ lib/ConstantsImages/ConstantsAudios.dart
@@ -0,0 +1,42 @@
+class ConstantsImages {
+  static const String audio_CityDayScene = 'assets/audios/1_1_Ciudad_sol.mpeg';
+  static const String audio_CityDayScene_ayuda =
+      'assets/audios/1_2_Ciudad_sol_mueve_felix.mpeg';
+  static const String audio_CityNightScene =
+      'assets/audios/2_1_Ciudad_sigue_luna.mpeg';
+  static const String audio_CityNightScene_ayuda =
+      'assets/audios/2_2_Ciudad_noche_mueve_a_felix.mpeg';
+  static const String audio_ForestNightScene =
+      'assets/audios/3_1_montañas_noche.mpeg';
+  static const String audio_ForestNightScene_ayuda =
+      'assets/audios/3_2_1_mueve_felix_hojas.mpeg';
+  static const String audio_ForestNightScene_secuencia =
+      'assets/audios/3_2_bosque_noche.mpeg';
+  static const String audio_ForestDayScene =
+      'assets/audios/4_1_Bosque_amaneciendo.mpeg';
+  static const String audio_ForestDayScene_ayuda =
+      'assets/audios/4_2_mueve_felix_bosque.mpeg';
+  static const String audio_RiverScene =
+      'assets/audios/5_1_bosque_rio_inical.mpeg';
+  static const String audio_RiverScene_Error =
+      'assets/audios/5_1_eleccion_erronea.mpeg';
+  static const String audio_RiverWoodScene =
+      'assets/audios/6_1_cruzando_rio.mpeg';
+  static const String audio_RiverWoodScene_ayuda =
+      'assets/audios/6_1_mueve_Felix_rio.mpeg';
+  static const String audio_MountainDessertScene =
+      'assets/audios/7_1_Elegir_camino_ayuda.mpeg';
+  static const String audio_MountainDessertScene_Error =
+      'assets/audios/7_3_Camino_error_desierto.mpeg';
+  static const String audio_MountainScene =
+      'assets/audios/8_1_montañas_rodear.mpeg';
+  static const String audio_MountainScene_ayuda =
+      'assets/audios/8_2_Ayuda_mover_montañas.mpeg';
+  static const String audio_MountainCityScene =
+      'assets/audios/9_1_Montañas_o_ciudad.mpeg';
+  static const String audio_MountaincityScene_Error =
+      'assets/audios/9_2_Elegir_camino_error_montaña.mpeg';
+  static const String audio_EndScene = 'assets/audios/10_1_ciudad_fin.mpeg';
+}
+
+ConstantsImages character = ConstantsImages();
